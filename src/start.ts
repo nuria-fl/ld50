4b90f16673b5c5bcba748c6814391905257947bc
Nino Dafonte <nino@dafonte.dev>
2022-04-08 18:11:18 +0200

    feat: add bg music

--- src/start.ts
+++ src/start.ts
@@ -3,9 +3,21 @@ export default class Start extends Phaser.Scene {
     super("start");
   }
 
-  preload() {}
+  preload() {
+    this.load.audio("bg_music_menu", [
+      "assets/audio/bg_music_intro.m4a",
+      "assets/audio/bg_music_intro.mp3",
+      "assets/audio/bg_music_intro.ogg",
+    ]);
+  }
 
   create() {
+    var music = this.sound.add("bg_music_menu");
+    music.play({
+      loop: true,
+      volume: 0.1,
+    });
+
     this.add
       .text(270, 270, "Start Game", {
         fontFamily: "Courier New",
